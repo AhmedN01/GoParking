5ceac8344ec210df3875df8b2f2b9cfb7e73a2fd
AhmedN01 <161676261+AhmedN01@users.noreply.github.com>
2024-05-12 22:26:17 +1000

    Language

---
+++ Data_Attribution.dart
@@ -0,0 +1,175 @@
+// ignore_for_file: file_names
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../theme/theme_manager.dart';
+
+class Dataattribution extends StatefulWidget {
+  const Dataattribution({Key? key}) : super(key: key);
+
+  @override
+  State<Dataattribution> createState() => _DataattributionState();
+}
+
+class _DataattributionState extends State<Dataattribution> {
+
+  getdarkmodepreviousstate() async {
+    final prefs = await SharedPreferences.getInstance();
+    bool? previusstate = prefs.getBool("setIsDark");
+    if (previusstate == null) {
+      notifire.setIsDark = false;
+    } else {
+      notifire.setIsDark = previusstate;
+    }
+  }
+  late ColorNotifire notifire;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getdarkmodepreviousstate();
+  }
+  @override
+  Widget build(BuildContext context) {
+    notifire = Provider.of<ColorNotifire>(context, listen: true);
+    return Scaffold(
+      appBar: AppBar(
+        leading: BackButton(
+          color:notifire.getwhiteblackcolor,
+        ),
+        automaticallyImplyLeading: true,
+        backgroundColor:notifire.getbgcolor,
+        elevation: 0,
+        centerTitle: true,
+        title: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Text(
+              'Account Security',
+              style: TextStyle(
+                  fontFamily: 'Gilroy',
+                  fontWeight: FontWeight.w700,
+                  fontSize: 18,
+                  color:notifire.getwhiteblackcolor),
+            ),
+          ],
+        ),
+      ),
+      backgroundColor: notifire.getbgcolor,
+      body: SingleChildScrollView(
+        child:  Column(
+          children: [
+            Padding(
+              padding:  const EdgeInsets.symmetric(horizontal: 25),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                      '1. Terms',
+                      style: TextStyle(
+                          fontFamily: 'Gilroy',
+                          fontWeight: FontWeight.w700,
+                          fontSize: 18,
+                          color:notifire.getwhiteblackcolor)
+                  ),
+                  SizedBox(
+                    height: Get.height*0.02,
+                  ),
+                  const Text(
+                      'This is Macquaire car park\n',
+                          
+                      style: TextStyle(
+                          fontFamily: 'Gilroy',
+                          fontWeight: FontWeight.w400,
+                          fontSize: 14,
+                          color:Colors.grey)
+                  ),
+                  SizedBox(
+                    height: Get.height*0.02,
+                  ),
+                  const Text(
+                       'This is Macquaire car park\n',
+                      style: TextStyle(
+                          fontFamily: 'Gilroy',
+                          fontWeight: FontWeight.w400,
+                          fontSize: 14,
+                          color:Colors.grey)
+                  ),
+                  SizedBox(
+                    height: Get.height*0.02,
+                  ),
+                  Text(
+                      '2. Changes to the Service and/or Terms:',
+                      style: TextStyle(
+                          fontFamily: 'Gilroy',
+                          fontWeight: FontWeight.w700,
+                          fontSize: 16,
+                          color:notifire.getwhiteblackcolor)
+                  ),
+                  SizedBox(
+                    height: Get.height*0.02,
+                  ),
+                  const Text(
+                       'This is Macquaire car park\n',
+                      style: TextStyle(
+                          fontFamily: 'Gilroy',
+                          fontWeight: FontWeight.w400,
+                          fontSize: 14,
+                          color:Colors.grey)
+                  ),
+                  SizedBox(
+                    height: Get.height*0.02,
+                  ),
+                  const Text(
+                       'This is Macquaire car park\n',
+                      style: TextStyle(
+                          fontFamily: 'Gilroy',
+                          fontWeight: FontWeight.w400,
+                          fontSize: 14,
+                          color:Colors.grey)
+                  ),
+                  SizedBox(
+                    height: Get.height*0.02,
+                  ),
+                  Text(
+                      '3. Create an Acknowledgment Statement',
+                      style: TextStyle(
+                          fontFamily: 'Gilroy',
+                          fontWeight: FontWeight.w700,
+                          fontSize: 16,
+                          color: notifire.getwhiteblackcolor)
+                  ),
+                  SizedBox(
+                    height: Get.height*0.02,
+                  ),
+                  const Text(
+                       'This is Macquaire car park\n',
+                      style: TextStyle(
+                          fontFamily: 'Gilroy',
+                          fontWeight: FontWeight.w400,
+                          fontSize: 14,
+                          color:Colors.grey)
+                  ),
+                  SizedBox(
+                    height: Get.height*0.02,
+                  ),
+                  const Text(
+                       'This is Macquaire car park\n',
+                      style: TextStyle(
+                          fontFamily: 'Gilroy',
+                          fontWeight: FontWeight.w400,
+                          fontSize: 14,
+                          color:Colors.grey)
+                  ),
+                ],
+              ),
+            ),
+
+          ],
+        ),
+      ),
+    );
+  }
+}
