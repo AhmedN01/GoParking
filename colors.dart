eba6829a11e7a9dfba7563bfc6630dfc7622a850
AhmedN01 <161676261+AhmedN01@users.noreply.github.com>
2024-05-12 22:19:33 +1000

    Theme Manager
    Theme and color files

---
+++ colors.dart
@@ -0,0 +1,30 @@
+// ignore_for_file: file_names, non_constant_identifier_names, prefer_const_constructors
+
+
+import 'package:flutter/material.dart';
+Color WhiteColor = const Color(0xffFFFFFF);
+Color BlackColor = const Color(0xff000000);
+Color bgcolor = Color(0xffF9FBFC);
+Color BlackColor2 = Color.fromARGB(255, 49, 15, 15);
+Color greyColor = const Color(0xffA7AEC1);
+Color greyColor2 = const Color(0xffA7AEC1);
+Color Darkblue = const Color(0xff3D5BF6);
+Color Darkblue2 = const Color(0xff3D5BF6);
+Color yelloColor = const Color(0xffFFBB0D);
+Color yelloColor2 = const Color(0xffFFBB0D);
+Color RedColor = const Color(0xffFF4747);
+Color RedColor2 = const Color(0xffFF4747);
+Color lightgrey = const Color(0xffDDDDDD);
+Color lightgrey2 = const Color(0xffDDDDDD);
+// ignore: use_full_hex_values_for_flutter_colors
+Color lightBlack = const Color(0xff73000000);
+Color lightBlack2 = const Color(0XFF636777);
+Color onoffColor = const Color(0xffE7E7E7);
+Color onoffColor2 = const Color(0xffE7E7E7);
+Color darkmode = const Color(0xff111315);
+Color boxcolor = const Color(0xff202427);
+Color textxolor=const Color(0xff75788D);
+Color selectcolor=const Color(0xff2378EF);
+Color semcolors=const Color(0xffF9FBFC);
+Color greyprofile=const Color(0xffD9D9D9);
+Color greytext=const Color(0xffC4C4C4);
